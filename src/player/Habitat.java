e4acff1e58c5bc9afb2958e2c7e8d0cc5380ad4a
NUVA <102314039+NuvaGit@users.noreply.github.com>
2023-02-02 12:35:14 +0000

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/Habitats/dessert.png 	new file:   bin/Habitats/forest.png 	new file:   bin/Habitats/marine.png 	new file:   bin/application/GameMain.class 	new file:   bin/application/GamePanel.class 	new file:   bin/application/Main.class 	new file:   bin/application/Main.fxml 	new file:   bin/application/Scene2.class 	new file:   bin/application/Scene2.fxml 	new file:   bin/application/Scene3.fxml 	new file:   bin/application/Scene4.fxml 	new file:   bin/application/application.css 	new file:   bin/application/scene3.class 	new file:   bin/application/scene4.class 	new file:   bin/module-info.class 	new file:   bin/player/Habitat.class 	new file:   bin/player/player.class 	new file:   build.fxbuild 	new file:   src/Habitats/dessert.png 	new file:   src/Habitats/forest.png 	new file:   src/Habitats/marine.png 	new file:   src/application/GameMain.java 	new file:   src/application/GamePanel.java 	new file:   src/application/Main.fxml 	new file:   src/application/Main.java 	new file:   src/application/Scene2.fxml 	new file:   src/application/Scene2.java 	new file:   src/application/Scene3.fxml 	new file:   src/application/Scene4.fxml 	new file:   src/application/application.css 	new file:   src/application/scene3.java 	new file:   src/application/scene4.java 	new file:   src/module-info.java 	new file:   src/player/Habitat.java 	new file:   src/player/player.java

---
+++ src/player/Habitat.java
@@ -0,0 +1,18 @@
+package player;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Habitat {
+	public BufferedImage image;
+
+ArrayList<String> habittats = new ArrayList<String>();
+public Habitat() {
+	habittats.add("marine waters");
+	habittats.add(" forests");
+	habittats.add("mountains");
+	habittats.add("rivers");
+	habittats.add("high deserts");
+
+}
+}

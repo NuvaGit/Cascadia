ccbfb7c50c2afd18007fb274c0f5c47262582937
NUVA <102314039+NuvaGit@users.noreply.github.com>
2023-02-22 17:17:43 +0000

    new file:   Cascadia---Group-18 	deleted:    bin/Habitats/board.png 	new file:   bin/Habitats/boardnew.png 	modified:   bin/Habitats/dessert.png 	modified:   bin/Habitats/forest.png 	new file:   bin/Habitats/forest_Dessert.png 	new file:   bin/Habitats/forest_river.png 	new file:   bin/Habitats/grass (2).png 	modified:   bin/Habitats/marine.png 	new file:   bin/Habitats/marine_dessert.png 	new file:   bin/Habitats/marine_forest.png 	new file:   bin/Habitats/marine_mountain.png 	new file:   bin/Habitats/mountain.png 	new file:   bin/Habitats/pixil-frame-0.png 	new file:   bin/Habitats/river.png 	new file:   bin/Habitats/river_dessert.png 	new file:   bin/Habitats/river_marine.png 	modified:   bin/application/Board.fxml 	new file:   bin/application/GaimMain3.class 	new file:   bin/application/GaimMain4.class 	modified:   bin/application/GameMain.class 	modified:   bin/application/GamePanel.class 	new file:   bin/application/Gamemain2.class 	modified:   bin/application/Main.class 	new file:   bin/application/grass.png 	new file:   bin/application/test.jpg 	modified:   bin/player/Habitat.class 	modified:   bin/player/player.class 	new file:   bin/player/wT.class 	deleted:    src/Habitats/board.png 	new file:   src/Habitats/boardnew.png 	modified:   src/Habitats/dessert.png 	modified:   src/Habitats/forest.png 	new file:   src/Habitats/forest_Dessert.png 	new file:   src/Habitats/forest_river.png 	new file:   src/Habitats/grass (2).png 	modified:   src/Habitats/marine.png 	new file:   src/Habitats/marine_dessert.png 	new file:   src/Habitats/marine_forest.png 	new file:   src/Habitats/marine_mountain.png 	new file:   src/Habitats/mountain.png 	new file:   src/Habitats/pixil-frame-0.png 	new file:   src/Habitats/river.png 	new file:   src/Habitats/river_dessert.png 	new file:   src/Habitats/river_marine.png 	modified:   src/application/Board.fxml 	new file:   src/application/GaimMain3.java 	new file:   src/application/GaimMain4.java 	modified:   src/application/GameMain.java 	modified:   src/application/GamePanel.java 	new file:   src/application/Gamemain2.java 	modified:   src/application/Main.java 	new file:   src/application/grass.png 	new file:   src/application/test.jpg 	modified:   src/player/Habitat.java 	modified:   src/player/player.java 	new file:   src/player/wT.java

--- src/application/GaimMain4.java
+++ src/application/GaimMain4.java
@@ -1,81 +1,59 @@
 package application;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import player.player;
 
-public class GameMain extends player {
+public class GaimMain4 extends player {
+
 	private static Scene scene;
 	static int turn = 1;
+
 	static ImageView lastClickedImage = null;
+
 	static {
 		try {
-			
-			
-			
-			
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-
-			//
-			//  Need to use the StartingWildlifeToken function for non starting tiles and create more cases for the split tiles 
-			
-			
-			//
-			
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			///-----------------------------------------------------------------------------------------------------------------
-			FXMLLoader loader = new FXMLLoader(Scene2.class.getResource("Board.fxml"));
-			//load up fxml file made with scene builder
 			player.shuffletiles();
-			// shuffle the tiles
+			FXMLLoader loader = new FXMLLoader(Scene2.class.getResource("Board.fxml"));
 			Parent root = loader.load();
-			Label label1 = (Label) root.lookup("#labelone"); //getting id and referncing label made
-			label1.setText("Player " + player.getName(0)); // set label to the first players name
-			Label label2 = (Label) root.lookup("#labeltwo"); //new labels initialised
-			Label label3 = (Label) root.lookup("#labelthree");
-			Label label4 = (Label) root.lookup("#labelfour");
-			Label label5 = (Label) root.lookup("#labelfive");
-			Label TurnLabel = (Label) root.lookup("#TurnLabel");
-			Label ScoreLabel = (Label) root.lookup("#ScoreLabel");
-			TurnLabel.setText("Turn: " + turn);
-			// ScoreLabel.setText("Score = " + player.getScore(player.Score));
-			ImageView startingimage = (ImageView) root.lookup("#startingimage");
 			ImageView image1 = (ImageView) root.lookup("#imageone");
 			ImageView image2 = (ImageView) root.lookup("#imagetwo");
 			ImageView image3 = (ImageView) root.lookup("#imagethree");
 			ImageView image4 = (ImageView) root.lookup("#imagefour");
+			ImageView startingimage = (ImageView) root.lookup("#startingimage");
+			Label label1 = (Label) root.lookup("#labelone");
+			label1.setText("Player " + player.getName(player.CurrentPlayer));
+			Button buttonone = (Button) root.lookup("#buttonone");
+
+			Label TurnLabel = (Label) root.lookup("#TurnLabel");
+			Label ScoreLabel = (Label) root.lookup("#ScoreLabel");
+			TurnLabel.setText("Turn: " + turn);
+
+			player.AddImageView(lastClickedImage);
+			ScoreLabel.setText("Score = " + player.getScore(player.Score));
+
+			Label label2 = (Label) root.lookup("#labeltwo");
+			Label label3 = (Label) root.lookup("#labelthree");
+			Label label4 = (Label) root.lookup("#labelfour");
+			Label label5 = (Label) root.lookup("#labelfive");
+			startingimage.setImage(player.getstarttile(player.CurrentPlayer));
+			image1.setImage(player.getTile(0));
+			image2.setImage(player.getTile(1));
+			image3.setImage(player.getTile(2));
+			image4.setImage(player.getTile(3));
+			label2.setText(player.gettilename(0));
+			label3.setText(player.gettilename(1));
+			label4.setText(player.gettilename(2));
+			label5.setText(player.gettilename(3));
+
+			// panes
 			ImageView pane2 = (ImageView) root.lookup("#pane2");
 			ImageView pane3 = (ImageView) root.lookup("#pane3");
 			ImageView pane4 = (ImageView) root.lookup("#pane4");
@@ -94,25 +72,10 @@ public class GameMain extends player {
 			ImageView pane18 = (ImageView) root.lookup("#pane17");
 			ImageView pane19 = (ImageView) root.lookup("#pane18");
 
-			Button buttonone = (Button) root.lookup("#buttonone");
-
 			Button rotateright = (Button) root.lookup("#rotateright");
 			Button rotateleft = (Button) root.lookup("#rotateleft");
 
 			ImageView test = (ImageView) root.lookup("#tester");
-			test.setImage(null);
-			startingimage.setImage(player.getstarttile(player.getcurrentPlayer()));
-			player.addmoretiles();
-			player.shuffletiles();
-			image1.setImage(player.getTile(0));
-			image2.setImage(player.getTile(1));
-			image3.setImage(player.getTile(2));
-			image4.setImage(player.getTile(3));
-			label2.setText(player.gettilename(0));
-			label3.setText(player.gettilename(1));
-			label4.setText(player.gettilename(2));
-			label5.setText(player.gettilename(3));
-			// ImageView lastClickedImage = null;
 
 			image1.setOnMouseClicked(event -> {
 				test.setOnMouseClicked(evernt -> {
@@ -311,7 +274,6 @@ public class GameMain extends player {
 					pane11.setImage(image3.getImage());
 					lastClickedImage = pane11;
 				});
-
 				pane12.setOnMouseClicked(e -> {
 					pane12.setImage(image3.getImage());
 					lastClickedImage = pane12;
@@ -426,6 +388,10 @@ public class GameMain extends player {
 
 			});
 
+			//
+			//
+			// rotate
+			//
 			rotateright.setOnAction(e -> {
 				lastClickedImage.setRotate(lastClickedImage.getRotate() + 90);
 
@@ -437,50 +403,21 @@ public class GameMain extends player {
 			});
 
 			buttonone.setOnAction(e -> {
-
-				// ment to add to arralist to do score
-				player.AddImageView(lastClickedImage);
-
-				// the one above is ment to so that
-				// ------------------------------------------------------
-				// ------------------------------------------------------
 				player.shuffletiles();
+
+				player.AddImageView(lastClickedImage);
 				ScoreLabel.setText("Score = " + player.getScore(player.Score));
+
 				turn = turn + 1;
 				TurnLabel.setText("Turn: " + turn);
-				label2.setText(player.gettilename(0));
-				label3.setText(player.gettilename(1));
-				label4.setText(player.gettilename(2));
-				label5.setText(player.gettilename(3));
-				image1.setImage(player.getTile(0));
-				image2.setImage(player.getTile(1));
-				image3.setImage(player.getTile(2));
-				image4.setImage(player.getTile(3));
 				player.CurrentPlayer = player.CurrentPlayer + 1;
 
 				Stage stage = (Stage) buttonone.getScene().getWindow();
-				stage.setScene(Gamemain2.getScene());
+				stage.setScene(GameMain.getScene());
 				stage.show();
 
 			});
 
-			/*
-			 * image1.setOnDragDetected(event -> { Dragboard dragboard =
-			 * image1.startDragAndDrop(TransferMode.MOVE); ClipboardContent content = new
-			 * ClipboardContent(); content.putImage(image1.getImage());
-			 * dragboard.setContent(content); event.consume(); });
-			 * 
-			 * 
-			 * pane1.setOnDragOver(event -> { event.acceptTransferModes(TransferMode.MOVE);
-			 * event.consume(); });
-			 * 
-			 * pane1.setOnDragDropped(event -> { Dragboard dragboard = event.getDragboard();
-			 * Image image = (Image) dragboard.getContent(format); ImageView imageView = new
-			 * ImageView(image); imageView.setFitWidth(pane1.getWidth());
-			 * imageView.setFitHeight(pane1.getHeight());
-			 * pane1.getChildren().add(imageView); event.setDropCompleted(true);
-			 * event.consume(); });
-			 */
 			scene = new Scene(root, 800, 600);
 		} catch (IOException e) {
 			e.printStackTrace();
